ff2dc5d8b496f71fd67d2bfe66b01f14500b4c4f
x-Beo-x <86937244+x-Beo-x@users.noreply.github.com>
2021-11-03 16:53:47 +0700

    first commit

---
+++ src/main/java/com/example/casestudyecommerce/CaseStudyECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.casestudyecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudyECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseStudyECommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/casestudyecommerce/CaseStudyECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.casestudyecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudyECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
