9869748b4e2e2160030de2833760c784b05d2c2a
vanhopld98 <vanhopld98@gmail.com>
2021-11-05 09:35:29 +0700

    update function findProductByCategory

--- src/main/java/com/casestudyecommerce/restController/category/CategoryController.java
+++ src/main/java/com/casestudyecommerce/restController/category/CategoryController.java
@@ -2,6 +2,8 @@ package com.casestudyecommerce.restController.category;
 
 import com.casestudyecommerce.category.Category;
 import com.casestudyecommerce.category.ICategoryService;
+import com.casestudyecommerce.product.IProductService;
+import com.casestudyecommerce.product.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,10 +14,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/category")
+@RequestMapping("/categories")
 public class CategoryController {
     @Autowired
     private ICategoryService categoryService;
+
+    @Autowired
+    private IProductService productService;
 
     @GetMapping
     public ResponseEntity<Page<Category>> findAll(Pageable pageable) {
@@ -26,13 +31,13 @@ public class CategoryController {
         return new ResponseEntity<>(categoryPage, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Category> findById(@PathVariable Long id) {
+    @GetMapping("/{id}/products")
+    public ResponseEntity<Page<Product>> findProductByCategory(@PathVariable Long id, Pageable pageable) {
         Optional<Category> categoryOptional = categoryService.findById(id);
         if (!categoryOptional.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(categoryOptional.get(), HttpStatus.OK);
+        return new ResponseEntity<>(productService.findAllByCategory(categoryOptional.get(),pageable), HttpStatus.OK);
     }
 
     @PostMapping
