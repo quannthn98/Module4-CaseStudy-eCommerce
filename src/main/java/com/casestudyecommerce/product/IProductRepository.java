a517baa9a6356b3c3f33c0baaabbdeb03461de74
vanhopld98 <vanhopld98@gmail.com>
2021-11-05 09:34:57 +0700

    update ProductService , Repo

--- src/main/java/com/casestudyecommerce/product/IProductRepository.java
+++ src/main/java/com/casestudyecommerce/product/IProductRepository.java
@@ -1,5 +1,7 @@
 package com.casestudyecommerce.product;
 
+import com.casestudyecommerce.brand.Brand;
+import com.casestudyecommerce.category.Category;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +11,7 @@ import org.springframework.stereotype.Repository;
 public interface IProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByNameContaining(String name, Pageable pageable);
 
-    Page<Product> findAllByBrandContaining(String brand, Pageable pageable);
+    Page<Product> findAllByBrand(Brand brand, Pageable pageable);
+
+    Page<Product> findAllByCategory(Category category, Pageable pageable);
 }
