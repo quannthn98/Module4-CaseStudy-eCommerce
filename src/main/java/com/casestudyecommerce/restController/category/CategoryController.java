5b532ad5fa1fbab66870bd3cd3aec9c3dc31dc2a
vanhopld98 <vanhopld98@gmail.com>
2021-11-05 13:54:46 +0700

    update CategoryController CrossOrigin

--- src/main/java/com/casestudyecommerce/restController/category/CategoryController.java
+++ src/main/java/com/casestudyecommerce/restController/category/CategoryController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/categories")
 public class CategoryController {
     @Autowired
